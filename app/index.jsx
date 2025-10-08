6d364dd2e19cf25b71effe5a746cf8c62e8336ba
Shakil Ahammed <shaakilahammed@gmail.com>
2025-10-08 18:51:25 +0600

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and prettier-plugin-tailwindcss to package.json
    - Updated react-native-reanimated and react-native-safe-area-context versions
    - Added tailwind.config.js for Nativewind configuration
    - Updated tsconfig.json to include nativewind-env.d.ts and application entry points

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-accent">Welcome to the Movie App</Text>
+    </View>
+  );
+}
